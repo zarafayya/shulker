3eda1ba987f4da7f1c2705302477ff2b83be506d
ink0rr <tomflynnbeltsazar@gmail.com>
2024-08-15 20:14:44 +0700

    Deprecate sleep function

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -2,9 +2,10 @@ import { system } from "@minecraft/server";
 
 /**
  * Sleeps for the specified amount of time
- * @param tick Amount of time, in ticks
+ * @param ticks Amount of time, in ticks
  * @returns Promise that resolves after the specified amount of time
+ * @deprecated Use `system.waitTicks` instead
  */
-export function sleep(tick: number) {
-  return new Promise<void>((resolve) => system.runTimeout(resolve, tick));
+export function sleep(ticks: number) {
+  return system.waitTicks(ticks);
 }
