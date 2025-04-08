ea507946696e050bc592e3724267a50456d29603
zarafayya <zahra.fayyadiyati@gmail.com>
2025-04-08 13:33:23 +0700

    fix: isActiveChunk type

--- src/utils/active_chunk.ts
+++ src/utils/active_chunk.ts
@@ -1,15 +1,17 @@
-import { Vector3, world } from "@minecraft/server";
+import { MinecraftDimensionTypes, type Vector3, world } from "@minecraft/server";
 
-type DimensionType = "minecraft:nether" | "minecraft:overworld" | "minecraft:the_end";
+type DimensionId =
+  | (typeof MinecraftDimensionTypes)[keyof typeof MinecraftDimensionTypes]
+  | (string & {});
 
 /**
  * Check if the location is in an active chunk.
  * @param location - The location to check.
  * @returns True if the location is in an active chunk.
  */
-export function isActiveChunk(dimension: DimensionType, location: Vector3): boolean {
+export function isActiveChunk(dimension: DimensionId, location: Vector3): boolean {
   try {
-    return !!world.getDimension(dimension).getBlock(location);
+    return !!world.getDimension(dimension as string).getBlock(location);
   } catch {
     return false;
   }
