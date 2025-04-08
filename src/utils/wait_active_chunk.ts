cab97ed18b435d904a4cebe9c4ac22d52f0e10e3
zarafayya <zahra.fayyadiyati@gmail.com>
2025-04-08 21:15:38 +0700

    feat(wip): active chunk

--- src/utils/wait_active_chunk.ts
+++ src/utils/wait_active_chunk.ts
@@ -1,10 +1,12 @@
 import { Dimension, system, Vector3 } from "@minecraft/server";
 import { isActiveChunk } from "./active_chunk.js";
 
-system.runInterval(() => {});
-
-// export function waitActiveChunk(callback: () => void, location: Vector3, dimension: Dimension) {
-//   while (isActiveChunk(dimension.id, location)) {
-    
-//   }
-// }
+export function waitActiveChunk(callback: () => void, location: Vector3, dimension: Dimension) {
+  const runId = system.runTimeout(() => {
+    if (isActiveChunk(dimension.id, location)) {
+      callback();
+      system.clearRun(runId);
+    }
+  }, 1);
+  return runId;
+}
